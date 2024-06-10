988f8f69b852fc43179bc7e51eef68f885b25cf3
Joseph Akaro <josephmmakaro@gmail.com>
2024-06-10 23:05:15 +0300

    Database setup

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def shutdown_session(exception=None):
 if __name__ == '__main__':
     with app.app_context():
         init_db()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
