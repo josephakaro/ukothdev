8cdb93ec8aba0273b452282bdc4bd4de11b29976
Joseph Akaro <josephmmakaro@gmail.com>
2024-06-29 01:25:08 +0300

    Backend Reset

--- app/pages/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-# Create a Blueprint object
-home = Blueprint('home', __name__)
-
-@home.route('/')
-def index():
-    return render_template('index.html')
--- app/pages/thanks.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-thanks = Blueprint('thanks', __name__)
-
-@thanks.route('/thank_you')
-def thank():
-    return render_template('thanks.html')
