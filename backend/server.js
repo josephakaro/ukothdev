ec6f172585c9dbb52036b23c2d239155219e22e6
Joseph Akaro <josephmmakaro@gmail.com>
2024-06-30 19:21:52 +0300

    Basic Backend logic and Frontend setup

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,21 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const sequelize = require('./config/database');
 
 dotenv.config();
 
 const app = express();
 
 require('./config/passport');
+
+app.use(
+	cors({
+		origin: 'http://127.0.0.1:5173', // Ensure this matches your frontend URL
+		credentials: true,
+	})
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,10 +30,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', require('./routes/auth'));
+app.use('/auth', require('./routes/auth'));
 app.use('/digitalocean', require('./routes/digitaloceanRoute'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
+sequelize
+	.sync()
+	.then(() => {
+		const PORT = process.env.PORT || 3000;
+		app.listen(PORT, () => {
+			console.log(`Server running on port ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error('Unable to connect to the database:', error);
+	});
